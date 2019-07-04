8c3c80341f78ddba84c0a04b22586840af249e69
misha <shma2001@gmail.com>
2019-07-04 22:36:10 +0300

    scripts

--- kafka-dispatch-tack/src/main/java/org.misha/dispatch/tack/TickListener.java
+++ kafka-dispatch-tack/src/main/java/org.misha/dispatch/tack/TickListener.java
@@ -2,12 +2,10 @@ package org.misha.dispatch.tack;
 
 import org.misha.dispatch.Listener;
 import org.misha.dispatch.Sender;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 class TickListener extends Listener {
 
-    @Autowired
     public TickListener(Sender tackSender) {addObserver(tackSender);}
 }
